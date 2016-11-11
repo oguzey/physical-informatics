67112c89315389e5a5c91a8d0ad5ee6f4da14f7d
Oleg Guzey <oguzey@cybervisiontech.com>
2016-11-11 22:23:06 +0200

    lab5 in progress

--- lab5_atraktor.py
+++ lab5_atraktor.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 # do some fancy 3D plotting
 from mpl_toolkits.mplot3d import Axes3D
+import logging
+import sys
+
+sh = logging.StreamHandler(sys.stdout)
+sh.setFormatter(logging.Formatter('%(asctime)s: %(levelname)s: %(message)s'))
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logger.addHandler(sh)
 
 
 def make_figure(name):
@@ -31,22 +39,41 @@ def Lorenz(state, t):
     return [xd, yd, zd]
 
 
-state0 = [1.0, 1.0, 1.0]
-t = np.arange(0.0, 250.0, 0.02)
-state = odeint(Lorenz, state0, t)
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.plot(state[:, 0], state[:, 1], state[:, 2])
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+def draw_original_system(system_states):
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot(system_states[:, 0], system_states[:, 1], system_states[:, 2])
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
 
-make_figure('Y1(t)')
-plt.title('Y1(t)')
-plt.plot(t, state[:, 0], 'o-', label='Y1(t)')
-plt.grid(b=True, which='major', color='grey', linestyle='--')
-# def autocorr(x):
-#     result = numpy.correlate(x, x, mode='full')
-#     return result[result.size/2:]
+
+def draw_y1_t(states_y1):
+    make_figure('Y1(t)')
+    plt.title('Y1(t)')
+    plt.plot(t, states_y1, 'o-', label='Y1(t)')
+    plt.grid(b=True, which='major', color='grey', linestyle='--')
+
+
+delta_t = 0.02
+T = 250.0
+T_ini = 0.0
+t = np.arange(T_ini, T, delta_t)
+states = odeint(Lorenz, [1.0, 1.0, 1.0], t)
+
+# draw_original_system(states)
+
+# step 2
+draw_y1_t(states[:, 0])
+# redefine Tini as 18
+T_ini = 18.0
+
+# step 3
+N_max = T / delta_t
+logger.info('N max equal to %f', N_max)
+
+# step 4
+
+
 
 plt.show()
